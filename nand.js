a218e13fc638151305b4e5fa543d06ea85039bfa
atmarksharp <atmarksharp@live.jp>
2015-11-01 22:00:55 +0900

    Implemented Nand#parse()

--- nand.js
+++ nand.js
@@ -4,30 +4,111 @@
 
   // Nand Class
   var Nand = {
-    'SExp': function(list){
-      this.list = list || [];
+    'STree': {
+      'SRoot': function(list){
+        this.list = list || [];
+        this.type = "SDoc";
+      },
+      'SExpr': function(list){
+        this.list = list || [];
+        this.type = "SExpr";
+      },
+      'LParen': function(){
+        this.type = "LParen";
+      },
+      'Id': function(name){
+        this.name = name;
+        this.type = "Id";
+      },
+      'Bool': function(value){
+        this.value = value;
+        this.type = "Bool";
+      },
     },
-    'parse': function(s){
-      for(var i=0; i<s.length; ++i){
-        var c = s[i];
-        // console.log(c);
+
+    'parse': function(code){
+      var is_id = function(s){
+        var re = /^[a-zA-Z_][a-zA-Z_0-9]*$/;
+        return (re.test(s));
       }
+      var is_bool = function(c){
+        return c === "0" || c === "1";
+      }
+      var is_blank = function(c){
+        return /[\s]/.test(c) || c == "\n" || c == "\r";
+      }
+
+      var reg = [];
+      var len = code.length;
+
+      for (var i=0; i < len; ++i){
+        var c = code[i];
+
+        if(c === "("){ // LPAREN
+          reg.push( new Nand.STree.LParen() );
+
+        }else if(c === ")"){ // RPAREN
+
+          // construct SExpr
+          var list = [];
+          while(true){
+            var last = reg.length-1;
+            var e = reg[last];
+
+            if(e instanceof Nand.STree.LParen){
+              reg.pop();
+              reg.push(new Nand.STree.SExpr(list));
+              break;
+            }else{
+              list.unshift(reg.pop());
+            }
+          }
+
+        }else if(is_id(c)){ // ID
+          var s = c;
+          ++i;
+          while(i<len && is_id(s+code[i])){
+            s += code[i];
+            ++i;
+          }
+          --i;
+          reg.push(new Nand.STree.Id(s));
+
+        }else if(is_bool(c)){ // BOOL
+          var v = (c=="0")? 0: 1;
+          reg.push(new Nand.STree.Bool(v));
+
+        }else if(is_blank(c)){ // BLANK
+          continue; //skip
+        }else{
+          return null;
+        }
+      }
+      return new Nand.STree.SRoot(reg); // End of #parse
     },
+
     'eval': function(code){
       var tree = Nand.parse(code);
     }
-  }
+  };
 
-  // Methods of SExp
-  Nand.SExp.prototype.get = function(i){
+  // Methods of STree
+
+  // SExpr
+  Nand.STree.SExpr.prototype.get = function(i){
     return this.list[i];
-  }
-  Nand.SExp.prototype.set = function(i,v){
+  };
+  Nand.STree.SExpr.prototype.set = function(i,v){
     return this.list[i] = v;
-  }
-  Nand.SExp.prototype.add = function(v){
+  };
+  Nand.STree.SExpr.prototype.add = function(v){
     return this.list.push(v);
-  }
+  };
+  // SRoot
+  Nand.STree.SRoot.prototype.get = function(i){
+    return this.list[i];
+  };
+
 
   // =======================
   //  DO NOT CHANGE BELOW!!
--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@
 var Nand = require("../nand.js");
 var assert = require('assert');
 
-describe("SExp", function(){
-  it("new SExp()", function(){
-    var e = new Nand.SExp();
+describe("Nand.STree.SExpr", function(){
+  it("new SExpr()", function(){
+    var e = new Nand.STree.SExpr();
     assert.equal('object', typeof e);
     assert.equal('function', typeof e.get);
     assert.equal('function', typeof e.set);
@@ -14,17 +14,17 @@ describe("SExp", function(){
   });
 
   it("#get()", function(){
-    var e = new Nand.SExp();
+    var e = new Nand.STree.SExpr();
     assert.equal(null, e.get(0));
 
-    var e2 = new Nand.SExp(["foo","bar"]);
+    var e2 = new Nand.STree.SExpr(["foo","bar"]);
     assert.equal(2, e2.list.length);
     assert.equal("foo", e2.get(0));
     assert.equal("bar", e2.get(1));
   });
 
   it("#set()", function(){
-    var e = new Nand.SExp();
+    var e = new Nand.STree.SExpr();
     e.set(0, "a");
     e.set(1, "1");
     e.set(2, "0");
@@ -35,7 +35,7 @@ describe("SExp", function(){
   });
 
   it("#add()", function(){
-    var e = new Nand.SExp();
+    var e = new Nand.STree.SExpr();
     e.add("a");
     e.add("1");
     e.add("0");
@@ -48,18 +48,23 @@ describe("SExp", function(){
 
 describe("#parse()", function(){
   it("and", function(){
-    var e = Nand.parse("(and 0 1)")
-    assert.equal("and", e.get(0));
-    assert.equal("0", e.get(1));
-    assert.equal("1", e.get(2));
+    var doc = Nand.parse("(and 0 1)")
+    assert.equal(true, doc instanceof Nand.STree.SRoot);
+
+    var e = doc.get(0);
+    console.log(e);
+    assert.equal(true, e instanceof Nand.STree.SExpr);
+    assert.equal(true, e.get(0) instanceof Nand.STree.Id);
+    assert.equal(true, e.get(1) instanceof Nand.STree.Bool);
+    assert.equal(true, e.get(2) instanceof Nand.STree.Bool);
   });
 });
 
-describe("#eval()", function(){
-  it("and", function(){
-    assert.equal("0", Nand.eval("(and 0 0)"));
-    assert.equal("0", Nand.eval("(and 1 0)"));
-    assert.equal("0", Nand.eval("(and 0 1)"));
-    assert.equal("1", Nand.eval("(and 1 1)"));
-  });
-});
+// describe("#eval()", function(){
+//   it("and", function(){
+//     assert.equal("0", Nand.eval("(and 0 0)"));
+//     assert.equal("0", Nand.eval("(and 1 0)"));
+//     assert.equal("0", Nand.eval("(and 0 1)"));
+//     assert.equal("1", Nand.eval("(and 1 1)"));
+//   });
+// });
